37e4ea7bd367717ca8e7c2a092e11f9d626855a3
Tom Hohl <tom.hohl@gmail.com>
2021-09-15 14:00:49 -0700

    models/db/seed data functional

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-Thing.create(name: Faker::Company.name)
-Thing.create(name: Faker::Company.name)
+
+Appointment.destroy_all
+Doctor.destroy_all
+User.destroy_all
+
+doctor1 = Doctor.create(name: Faker::Name.name)
+doctor2 = Doctor.create(name: Faker::Name.name)
+doctor3 = Doctor.create(name: Faker::Name.name)
+doctor4 = Doctor.create(name: Faker::Name.name)
+doctor5 = Doctor.create(name: Faker::Name.name)
+
+doctors = [doctor1, doctor2, doctor3, doctor4, doctor5]
+
+5.times do |i|
+    user = User.create(name: Faker::Name.name)
+    5.times do |i|
+        Appointment.create(date: Faker::Date.forward(days: rand(200)), doctor_id: doctors[rand(4)].id, user_id:user.id)
+    end
+end
+
+puts "Seeded #{Doctor.all.length} Doctors"
+puts "Seeded #{User.all.length} Users"
+puts "Seeded #{Appointment.all.length} Appointments"
